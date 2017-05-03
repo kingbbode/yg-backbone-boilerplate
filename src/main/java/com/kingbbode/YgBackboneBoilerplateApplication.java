8095393cf8af6d37ff54a07be104b3e765180ba9
권용근 <kingbbode@gmail.com>
2017-05-03 23:26:50 +0900

    on spring boot

---
+++ src/main/java/com/kingbbode/YgBackboneBoilerplateApplication.java
@@ -0,0 +1,12 @@
+package com.kingbbode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgBackboneBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgBackboneBoilerplateApplication.class, args);
+	}
+}
